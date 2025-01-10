54a155d1aca077163d881d3ab0b4680e3f0a70d7
hamza mraizik <hamzamraizik2004@gmail.com>
2025-01-10 02:49:46 +0100

    fixed ex00 in cpp02 , copy constructor understood

---
+++ cpp02/Fixed.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+class Fixed{
+    private:
+        int     integer;
+        static const int constInt = 8;
+    public:
+        Fixed();
+        Fixed(const Fixed& reference);
+        ~Fixed();
+        Fixed& operator=(Fixed& a);
+        int getRawBits( void ) const;
+        void setRawBits( int const raw );
+};
