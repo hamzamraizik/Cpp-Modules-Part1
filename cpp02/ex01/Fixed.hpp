fe80cc3068cb13713f9d9bd5587f5dae7f876a38
Hamza Mraizik <hmraizik@e3r6p1.1337.ma>
2025-01-12 23:06:02 +0100

    insertion operator overloaded and add the needed constructors

--- cpp02/ex01/Fixed.hpp
+++ cpp02/ex01/Fixed.hpp
@@ -4,16 +4,18 @@
 class Fixed{
     private:
         int     integer;
-        static const int constInt = 8;
+        static const int f_bits = 8;
     public:
         Fixed();
         Fixed(const Fixed& reference);
         Fixed(const int x);
         Fixed(const float x);
         ~Fixed();
-        Fixed& operator=(Fixed& a);
+        Fixed& operator=(const Fixed& a);
         int getRawBits( void ) const;
         void setRawBits( int const raw );
         float toFloat( void ) const;
         int toInt( void ) const;
 };
+
+std::ostream& operator<<(std::ostream& o_stream, const Fixed& a);
--- cpp02/ex01/main.cpp
+++ cpp02/ex01/main.cpp
@@ -11,15 +11,37 @@ Fixed::Fixed(const Fixed& reference){
     integer = reference.integer;
 }
 
-Fixed& Fixed::operator=(Fixed& a){
-    this->integer = a.integer;
+Fixed& Fixed::operator=(const Fixed& a){
     std::cout << "Copy assignment operator called" << std::endl;
+    this->integer = a.integer;
     return *this;
 }
 
 Fixed::~Fixed()
 {
     std::cout << "Destructor called" << std::endl;
+}
+
+Fixed::Fixed(const int x){
+    std::cout << "Int constructor called" << std::endl;
+    integer = x << f_bits;
+}
+Fixed::Fixed(const float x){
+    std::cout << "Float constructor called" << std::endl;
+    integer = x  * (1 << f_bits);
+}
+
+int Fixed::toInt(void) const{
+    return (integer >> f_bits);
+}
+
+float   Fixed::toFloat( void ) const {
+    return ((float)integer / (1 <<  f_bits));
+}
+
+std::ostream& operator<<(std::ostream& o_stream, const Fixed& a){
+    o_stream << a.toFloat();
+    return o_stream;
 }
 
 void  Fixed::setRawBits(int const raw ){
@@ -31,13 +53,19 @@ int Fixed::getRawBits(void ) const{
     return integer;
 }
 
-int main(){
-    Fixed a;
-    Fixed b(a);
-    Fixed c;
-
-    c = b;
-    std::cout << a.getRawBits() << std::endl;
-std::cout << b.getRawBits() << std::endl;
-std::cout << c.getRawBits() << std::endl;
+int main( void ) {
+Fixed a;
+Fixed const b( 10 );
+Fixed const c( 42.42f );
+Fixed const d( b );
+a = Fixed( 1234.4321f );
+std::cout << "a is " << a << std::endl;
+std::cout << "b is " << b << std::endl;
+std::cout << "c is " << c << std::endl;
+std::cout << "d is " << d << std::endl;
+std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+return 0;
 }
